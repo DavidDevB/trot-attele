06e77e4f9072366250aa81b83bad429f8bc5f218
DavidB <david.bernard@fms-ea.com>
2025-10-14 13:47:54 +0200

    Base version working

--- trot_attele_visuals.py
+++ trot_attele_visuals.py
@@ -9,24 +9,16 @@ def get_number_of_horses():
     return input("Choose the number of horses for this race: ")
 
 
-race_types = {"trifecta": 3, "quartet": 4, "quintet": 5}
-
-
 """ Retourne le type de la course (entré par le joueur) """
 def get_race_type():
-    while True:
-        race_type = input("Choose race type between trifecta, quartet, quintet: ")
-        if race_type not in race_types.keys():
-            print("Must be trifecta, quartet or quintet.")
-            race_type = input("Choose race type between trifecta, quartet, quintet: ")
-        return race_type
+    return input("Choose race type between trifecta, quartet, quintet: ")
 
 
 """ Retourne une liste de tous les chevaux participants (dictionnaires)"""
 def get_horses_specs():
     horses = []
     for i in range(int(get_number_of_horses())):
-        horses.append({"number": i + 1, "dist": 0, "speed": 0})
+        horses.append({"number": i + 1, "dist": 2400, "speed": 0})
     return horses
 
 
@@ -34,89 +26,80 @@ def get_horses_specs():
 def launch_race():
     horses = get_horses_specs()
     race_type = get_race_type()
-
+    race_types = {"trifecta": 3, "quartet": 4, "quintet": 5}
     winners = []
 
     # Boucle while permettant de boucler jusqu'à qu'il y est le nombre de gagnants indiqués par le type de course,
     # et jusqu'à qu'il n'y ait plus de cheval avec une distance supérieure à zéro.
-    while len(winners) < race_types[race_type] and any(horse["dist"] < 2400 for horse in horses):
-        for horse in horses[:]:
+    while len(winners) < race_types[race_type] and any(horse["dist"] > 0 for horse in horses):
+        for i in range(len(horses)):
             dice = random.choice(range(1, 7))
 
             # Switch match qui modifie la vitesse des chevaux selon le lancer de dé.
-            match horse["speed"]:
+            match horses[i]["speed"]:
                 case 0:
                     if dice in [2, 3, 4]:
-                        horse["speed"] += 1
+                        horses[i]["speed"] += 1
                     elif dice in [5, 6]:
-                        horse["speed"] += 2
+                        horses[i]["speed"] += 2
                 case 1:
                     if dice in [3, 4, 5]:
-                        horse["speed"] += 1
+                        horses[i]["speed"] += 1
                     elif dice == 6:
-                        horse["speed"] += 2
+                        horses[i]["speed"] += 2
                 case 2:
                     if dice in [3, 4, 5]:
-                        horse["speed"] += 1
+                        horses[i]["speed"] += 1
                     elif dice == 6:
-                        horse["speed"] += 2
+                        horses[i]["speed"] += 2
                 case 3:
                     if dice == 1:
-                        horse["speed"] -= 1
+                        horses[i]["speed"] -= 1
                     elif dice in [4, 5, 6]:
-                        horse["speed"] += 1
+                        horses[i]["speed"] += 1
                 case 4:
                     if dice == 1:
-                        horse["speed"] -= 1
+                        horses[i]["speed"] -= 1
                     elif dice in [5, 6]:
-                        horse["speed"] += 1
+                        horses[i]["speed"] += 1
                 case 5:
                     if dice == 1:
-                        horse["speed"] -= 2
+                        horses[i]["speed"] -= 2
                     elif dice == 2:
-                        horse["speed"] -= 1
+                        horses[i]["speed"] -= 1
                     elif dice == 6:
-                        horse["speed"] += 1
+                        horses[i]["speed"] += 1
                 case 6:
                     if dice == 1:
-                        horse["speed"] -= 2
+                        horses[i]["speed"] -= 2
                     elif dice == 2:
-                        horse["speed"] -= 1
+                        horses[i]["speed"] -= 1
                     elif dice == 6:
-                        horses.remove(horse)
-                        print(f"Horse number {horse["number"]} is disqualified!")
+                        horses[i]["speed"] = -1
 
             # Switch match qui modifie la distance de chaque cheval selon sa vitesse.
-            match horse["speed"]:
+            match horses[i]["speed"]:
                 case 0:
                     continue
                 case 1:
-                    horse["dist"] += 23
+                    horses[i]["dist"] -= 23
                 case 2:
-                    horse["dist"] += 46
+                    horses[i]["dist"] -= 46
                 case 3:
-                    horse["dist"] += 69
+                    horses[i]["dist"] -= 69
                 case 4:
-                    horse["dist"] += 92
+                    horses[i]["dist"] -= 92
                 case 5:
-                    horse["dist"] += 115
+                    horses[i]["dist"] -= 115
                 case 6:
-                    horse["dist"] += 138
+                    horses[i]["dist"] -= 138
 
             # Condition de victoire selon si le nombre de vainqueurs dans le tableau winners est inférieur ou supérieur au type de course,
             # et si le cheval à une distance inférieure ou égale à zéro.
-            if len(winners) < race_types[race_type] and horse not in winners:
-                if horse["dist"] >= 2400:
-                    winners.append(horse)
-                    print(f"{horse['number']} a terminé !")
-                if len(winners) >= race_types[race_type]:
-                    match race_type:
-                        case "trifecta":
-                            print(f"Winners are {winners[0]["number"]}, {winners[1]["number"]} et {winners[2]["number"]}!")
-                        case "quartet":
-                            print(f"Winners are {winners[0]["number"]}, {winners[1]["number"]}, {winners[2]["number"]} and {winners[3]["number"]}!")
-                        case "quintet":
-                            print(f"Winners are {winners[0]["number"]}, {winners[1]["number"]}, {winners[2]["number"]}, {winners[3]["number"]} and {winners[4]["number"]}!")
+            if len(winners) < race_types[race_type] and horses[i] not in winners:
+                if horses[i]["dist"] <= 0:
+                    winners.append(horses[i])
+                    print(f"{horses[i]['number']} a terminé !")
 
             # Condition qui arrête la boucle si le nombre de gagnants dans le tableau winners
             # est égal ou supérieur au type de course.
